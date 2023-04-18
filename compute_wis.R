45146d7a2a5dc98a62ae7b0e4c5e47a28ec8e850
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-04-18 12:00:46 +0200

    computed wis per 100k

--- compute_wis.R
+++ compute_wis.R
@@ -42,30 +42,45 @@ df <- load_data(
 
 df_national <- filter_data(df, level = "national")
 df_national <- compute_wis(df_national)
-write_csv(df_national, paste0("data/wis_national.csv.gz"))
+write_csv(df_national, "data/wis_national.csv")
 
 df_states <- filter_data(df, level = "states")
 df_states <- compute_wis(df_states)
-write_csv(df_states, paste0("data/wis_states.csv.gz"))
+write_csv(df_states, "data/wis_states.csv")
 
 df_age <- filter_data(df, level = "age")
 df_age <- compute_wis(df_age)
-write_csv(df_age, paste0("data/wis_age.csv.gz"))
+write_csv(df_age, "data/wis_age.csv")
 
 
 ### SHORT HORIZONS: 0-7 DAYS
 
 df_national_7d <- filter_data(df, level = "national", short_horizons = TRUE)
 df_national_7d <- compute_wis(df_national_7d)
-write_csv(df_national_7d, paste0("data/wis_national_7d.csv.gz"))
+write_csv(df_national_7d, "data/wis_national_7d.csv")
 
 df_states_7d <- filter_data(df, level = "states", short_horizons = TRUE)
 df_states_7d <- compute_wis(df_states_7d)
-write_csv(df_states_7d, paste0("data/wis_states_7d.csv.gz"))
+write_csv(df_states_7d, "data/wis_states_7d.csv")
 
 df_age_7d <- filter_data(df, level = "age", short_horizons = TRUE)
 df_age_7d <- compute_wis(df_age_7d)
-write_csv(df_age_7d, paste0("data/wis_age_7d.csv.gz"))
+write_csv(df_age_7d, "data/wis_age_7d.csv")
 
 
 ### PER 100K POPULATION
+
+df <- load_data(add_baseline = TRUE, add_median = TRUE, shorten_names = TRUE, fix_data = TRUE,
+                add_truth = TRUE, exclude_missing = TRUE, eval_date = "2022-08-08", per_100k = TRUE)
+
+df_national_100k <- filter_data(df, level = "national")
+df_national_100k <- compute_wis(df_national_100k)
+write_csv(df_national_100k, "data/wis_national_100k.csv")
+
+df_states_100k <- filter_data(df, level = "states")
+df_states_100k <- compute_wis(df_states_100k)
+write_csv(df_states_100k, "data/wis_states_100k.csv")
+
+df_age_100k <- filter_data(df, level = "age")
+df_age_100k <- compute_wis(df_age_100k)
+write_csv(df_age_100k, "data/wis_age_100k.csv")

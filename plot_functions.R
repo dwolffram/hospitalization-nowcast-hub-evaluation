c238f903a24d5ccf43b4e8eb28c84768a2632e08
dwolffram <25206417+dwolffram@users.noreply.github.com>
2023-04-20 15:43:47 +0200

    added rank plot

--- plot_functions.R
+++ plot_functions.R
@@ -1,4 +1,5 @@
 library(patchwork)
+library(ggridges)
 
 ### SCORES (AE, MSE, QS)
 
@@ -319,4 +320,53 @@ plot_coverage_lines <- function(df, level = "national") {
       title = TITLES[level]
     ) +
     theme(plot.title = element_text(hjust = 0.5))
+}
+
+
+### RANKS
+
+plot_ranks <- function(df, level = "national") {
+  temp <- filter_data(df, type = "quantile", level = level)
+  
+  df_rank <- temp %>%
+    group_by(location, age_group, target, target_end_date, model) %>%
+    summarize(mean_score = mean(score)) %>%
+    group_by(location, age_group, target, target_end_date) %>%
+    arrange(model, mean_score) %>%
+    mutate(rank = rank(mean_score, ties.method = "min")) %>%
+    arrange(target_end_date)
+  
+  m <- length(unique(df_rank$model))
+  
+  df_rank <- df_rank %>%
+    group_by(model) %>%
+    mutate(
+      rank = 1 - (rank - 1) / (m - 1),
+      meanRank = mean(rank)
+    )
+  
+  df_mean_rank <- df_rank %>%
+    group_by(model) %>%
+    summarize(rank = mean(rank))
+  
+  COLORS <- setNames(c("#440154", "#31688e", "#35b779", "#fde725"), 1:4)
+  
+  ggplot(df_rank, aes(
+    x = rank, y = reorder(model, meanRank),
+    fill = factor(stat(quantile))
+  )) +
+    stat_density_ridges(
+      geom = "density_ridges_gradient", calc_ecdf = TRUE,
+      quantiles = 4, quantile_lines = TRUE,
+      bandwidth = 0.075,
+      scale = 0.9,
+      alpha = 0.8
+    ) +
+    scale_fill_manual(name = "Quartile", values = alpha(COLORS, 0.6)) +
+    # scale_fill_viridis_d(name = "Quartiles", breaks = 1:4, alpha = 0.6) +
+    geom_point(data = df_mean_rank, aes(x = rank, y = model), inherit.aes = FALSE, shape = 3) +
+    xlab("Standardized rank") +
+    ylab("Model") +
+    xlim(c(0, 1)) +
+    theme_bw()
 }
---
+++ plot_ranks.R
@@ -0,0 +1,29 @@
+source("utils.R")
+source("plot_functions.R")
+
+df <- load_scores(load_baseline = FALSE)
+
+p1 <- plot_ranks(df, "national")
+p2 <- plot_ranks(df, "states")
+p3 <- plot_ranks(df, "age")
+
+t <- list(theme(
+  plot.title = element_text(size = 8, hjust = 0, face = "bold"),
+  legend.title = element_text(size = 6),
+  legend.text = element_text(size = 5),
+  legend.key.size = unit(0.8, "lines"),
+  strip.text = element_text(size = 8),
+  axis.title = element_text(size = 7),
+  axis.text = element_text(size = 6),
+  axis.ticks = element_line(colour = "black", size = 0.25),
+  panel.grid.major = element_line(size = 0.15),
+  panel.grid.minor = element_line(size = 0.1),
+  plot.margin = unit(c(2, 7, 2, 2), "pt"),
+  legend.background = element_rect(fill = "transparent")
+))
+
+(p1 + ylab(NULL) + theme(legend.position = "None") + facet_grid(~"National level") +
+  p2 + ylab(NULL) + theme(legend.position = "bottom") + facet_grid(~"States") +
+  p3 + ylab(NULL) + theme(legend.position = "None") + facet_grid(~"Age groups")) & t
+
+ggsave("figures/ranks.pdf", width = 164, height = 100, unit = "mm", device = "pdf")
